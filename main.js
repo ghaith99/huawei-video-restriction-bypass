97b075e92a028ebb59907cf77ac1c094449b1c29
Ghaith AlGhaith <gh4ith@gmail.com>
2019-10-24 14:52:26 +0300

    main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+    // ==UserScript==
+    // @name         Bypass Huawei Video Download Restrictions
+    // @namespace    http://tampermonkey.net/
+    // @version      0.1
+    // @description  inject mp4 video download link
+    // @author       Ghaith
+    // @match        https://ilearningx.huawei.com/courses/*
+    // @grant        none
+    // ==/UserScript==
+
+
+var timer;
+timer = setInterval(inject, 3000);
+function inject(){
+        if (typeof ($('.xblock-json-init-args')[1]) !== 'undefined') {
+            var options = JSON.parse($('.xblock-json-init-args')[1].innerHTML)
+            $.ajax({
+                notifyOnError: false,
+                url: '/vod/' + options["video_id"],
+                type: 'GET',
+                async: false,
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                success: function (res) {
+                        alert("inject2");
+                    $('<li class="video-download-button">\n' +
+                      '<a href="' + res['origin_url'] + '" download>Download the video</a>\n' +
+                      '</li>').appendTo(".videojs_block")[0];
+                    clearInterval(timer);
+                }
+            })
+        }
+}
+
+
+
