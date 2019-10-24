388ada149524d58fd2c69523d3c0ec897564d050
Ghaith AlGhaith <gh4ith@gmail.com>
2019-10-24 16:16:42 +0300

    Support for multi-section topics with multiple video downloads at the same time

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ will apppend download button under the video player, downloads mp4 file directly
 
 # How to use
 Paste in console or use Greasemonkey or TamperMonkey.
+
+
+![img](./img.png)
+
--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-    // ==UserScript==
-    // @name         Bypass Huawei Video Download Restrictions
-    // @namespace    http://tampermonkey.net/
-    // @version      0.1
-    // @description  inject mp4 video download link
-    // @author       Ghaith
-    // @match        https://ilearningx.huawei.com/courses/*
-    // @grant        none
-    // ==/UserScript==
+// ==UserScript==
+// @name         Bypass Huawei Video Download Restrictions
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  inject mp4 video download link
+// @author       Ghaith
+// @match        https://ilearningx.huawei.com/courses/*
+// @grant        none
+// ==/UserScript==
 
 
 var timer;
+var count = 1;
 timer = setInterval(inject, 3000);
 function inject(){
-        if (typeof ($('.xblock-json-init-args')[1]) !== 'undefined') {
-            var options = JSON.parse($('.xblock-json-init-args')[1].innerHTML)
+    $.each($('.xblock-json-init-args'),function(k,v){
+        if(v.innerHTML.includes("video_id")){
+            var options = JSON.parse(v.innerHTML)
             $.ajax({
                 notifyOnError: false,
                 url: '/vod/' + options["video_id"],
@@ -23,12 +25,14 @@ function inject(){
                 dataType: 'json',
                 success: function (res) {
                     $('<li class="video-download-button">\n' +
-                      '<a href="' + res['origin_url'] + '" download>Download the video</a>\n' +
+                      '<a href="' + res['origin_url'] + '" download>Download the video #'+count+'</a>\n' +
                       '</li>').appendTo(".videojs_block")[0];
+                    count += 1;
                     clearInterval(timer);
                 }
             })
         }
+    })
 }
 
 
